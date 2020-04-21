99caaba19a85f096afaf2990928188193f50ca70
ben <ben.hankins@ibm.com>
2020-04-21 12:33:10 -0500

    Hello World starts up

---
+++ src/components/list.jsx
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+//import './myCSS.css';
+
+class List extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {    
+    
+
+
+    return (
+      <div>
+            <h1>Hello World</h1>
+      </div>
+    )
+  }
+}
+
+export default List;
